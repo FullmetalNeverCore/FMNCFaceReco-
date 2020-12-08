719f084e5458d2fdd9cab873d79f441a33f28a75
FullmetalNeverCore <60969891+FullmetalNeverCore@users.noreply.github.com>
2020-12-08 07:14:40 +0200

    Update 2

--- facedetection.py
+++ facedetection.py
@@ -1,24 +1,38 @@
 import cv2 
+import schedule
+import time 
+import os 
 import sys
 
-imgpth = sys.argv[1]
-casc = sys.argv[2]
 
-fccs = cv2.CascadeClassifier(casc)
-img = cv2.imread(imgpth)
-gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgpth = "./1.png"
+casc = sys.argv[1]
 
-face = fccs.detectMultiScale(
-    gry,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
-print("FOUND ONE".format(len(face)))
+def TECH():
+    fccs = cv2.CascadeClassifier(casc)
+    img = cv2.imread(imgpth)
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-for (x,y,z,c) in face:
-    cv2.rectangle(img, (x, y), (x+z, y+c), (255, 0, 0), 2)
+    face = fccs.detectMultiScale(
+        gry,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    print("FOUND ONE".format(len(face)))
 
-cv2.imshow("Faces", img)
-cv2.waitKey(0)
+    for (x,y,z,c) in face:
+        cv2.rectangle(img, (x, y), (x+z, y+c), (255, 0, 0), 2)
+
+    cv2.imshow("Faces", img)
+    os.remove(imgpth)
+    cv2.waitKey(0)
+    
+
+schedule.every(1).minutes.do(TECH)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
